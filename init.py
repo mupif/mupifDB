7b20d92b2236a8552f5b6d7c8b9cb2c3c6dde4d3
Bořek Patzák <borek.patzak@gmail.com>
2020-07-15 16:32:42 +0200

    Improved db init script (not tested yet)

--- init.py
+++ init.py
@@ -4,11 +4,19 @@ import pymongo
 # run this only once to initialize MuPIF DB
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client.MuPIF
-usecases = db["UseCases"]
-usecases.insert_one({"_id":"DemoUseCase", "Description":"Demonstration use case})
-workflows= db["Workflows"]
+if (not db):
+    usecases = db["UseCases"]
+    usecases.insert_one({"_id":"DemoUseCase", "Description":"Demo UseCase"})
+    #workflows= db["Workflows"]
+    #workflowexecutions = db["WorkflowExecutions"]
+    #iodata = db["IOData"]
 
-workflowexecutions = db["WorkflowExecutions"]
-iodata = db["IOData"]
+    #force creation of empty collections
+    db.create_collection("Workflows")
+    db.create_collection("WorkflowExecutions")
+    db.create_collection("IOData")
+    print ("MuPIF DB init completed")
+else:
+    print ("MuPIF DB already exists, exiting")
 
 
