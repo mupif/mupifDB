d35e23694fad2cab1c1ba31fc326b7c383101fda
stanislavsulc <sulcstanda@seznam.cz>
2022-04-10 17:23:59 +0200

    Fix of setting inputs

--- webapi/index.py
+++ webapi/index.py
@@ -510,6 +510,7 @@ def setExecutionInputs(weid):
                             'Value': literal_eval(value),
                             'Time': None
                         }
+                        restApiControl.setExecutionInputLink(weid, name, objID, c_eid, c_name, c_objid)
                         restApiControl.setExecutionInputObject(weid, name, objID, object_dict)
                     else:
                         pass
