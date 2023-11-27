67300539df39173e25d9a0ab12a5d14a6768ccff
Václav Šmilauer <eu@doxos.eu>
2023-11-27 11:31:57 +0100

    add status2 to "safe" API

--- mupifDB/api/safeapi.py
+++ mupifDB/api/safeapi.py
@@ -3,6 +3,34 @@ import mupif as mp
 
 app = fastapi.FastAPI(openapi_tags=[{'name':'Stats'}])
 
+
+@app.get("/status2/", tags=["Stats"])
+def get_status2():
+    ns = None
+    try:
+        ns = mp.pyroutil.connectNameserver();
+        nameserverStatus = 'OK'
+    except:
+        nameserverStatus = 'Failed'
+
+    # get Scheduler status
+    schedulerStatus = 'Failed'
+    query = ns.yplookup(meta_any={"type:scheduler"})
+    try:
+        for name, (uri, metadata) in query.items():
+            s = Pyro5.api.Proxy(uri)
+            st = s.getStatistics()
+            schedulerStatus = 'OK'
+    except Exception as e:
+        print(str(e))
+
+    # get DMS status
+    if (client): DMSStatus = 'OK'
+    else: DMSStatus = 'Failed'
+
+    return {'nameserver': nameserverStatus, 'dms': DMSStatus, 'scheduler': schedulerStatus, 'name':os.environ["MUPIF_VPN_NAME"]}
+
+
 @app.get("/scheduler-status2/", tags=["Stats"])
 def get_scheduler_status2():
     ns = mp.pyroutil.connectNameserver();
