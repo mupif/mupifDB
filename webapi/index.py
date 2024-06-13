74d7776e500dd1c094cc7976069cea991f7fb028
stanislavsulc <sulcstanda@seznam.cz>
2024-06-13 15:58:38 +0200

    Google login

--- webapi/index.py
+++ webapi/index.py
@@ -215,7 +215,7 @@ def callback():
     token_url, headers, body = client.prepare_token_request(
         token_endpoint,
         authorization_response=request.url,
-        redirect_url=request.base_url,
+        redirect_url=GOOGLE_REDIRECT_URI,#request.base_url,
         code=code
     )
     token_response = requests.post(
