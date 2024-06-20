29de99a7efd0c81958ff2403088cccb7aade1d1d
stanislavsulc <sulcstanda@seznam.cz>
2024-06-20 19:17:34 +0200

    Fixing google login

--- webapi/index.py
+++ webapi/index.py
@@ -215,7 +215,7 @@ def callback():
     token_url, headers, body = client.prepare_token_request(
         token_endpoint,
         authorization_response=request.url,
-        redirect_url=request.base_url,
+        redirect_url=str(GOOGLE_REDIRECT_URI).replace('login/callback', ''),
         code=code
     )
     token_response = requests.post(
