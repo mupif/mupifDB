ab9aaa9e51514e8e0597844ba3cbdc50cc7da50d
Stanislav Šulc <sulcstanda@seznam.cz>
2025-08-28 15:47:07 +0200

    Adding workflow13 on database init.

---
+++ mupifDB/api/demo/w13.py
@@ -0,0 +1,102 @@
+import sys
+sys.path.extend(['..', '../..'])
+import threading
+
+threading.current_thread().name='ex13-main'
+
+import mupif as mp
+import Pyro5
+import logging
+
+log = logging.getLogger()
+
+
+@Pyro5.api.expose
+class Workflow13(mp.Workflow):
+
+    def __init__(self, metadata=None):
+        MD = {
+            "ClassName": "Workflow13",
+            "ModuleName": "main.py",
+            "Name": "Example13 workflow",
+            "ID": "workflow_13_demo",
+            "Timeout": 30,
+            "Description": "Calculates multiplication of two given values using a simple model",
+            "Inputs": [
+                {'Type': 'mupif.Property', 'Type_ID': 'mupif.DataID.PID_Time', 'Name': 'Value_1', "Obj_ID": '1',
+                 'Description': 'Input value 1', 'Units': 's', 'Required': True, "Set_at": "timestep", "ValueType": "Scalar"},
+                {'Type': 'mupif.Property', 'Type_ID': 'mupif.DataID.PID_Time', 'Name': 'Value_2', "Obj_ID": '2',
+                 'Description': 'Input value 2', 'Units': 's', 'Required': True, "Set_at": "timestep", "ValueType": "Scalar"},
+                {'Type': 'mupif.Property', 'Type_ID': 'mupif.DataID.PID_Time', 'Name': 'Delay', "Obj_ID": 'delay',
+                 'Description': 'Delay in seconds', 'Units': 's', 'Required': False, "Set_at": "timestep", "ValueType": "Scalar"}
+            ],
+            "Outputs": [
+                {'Type': 'mupif.Property', 'Type_ID': 'mupif.DataID.PID_Time', 'Name': 'Multiplication_result',
+                 'Description': 'Result of multiplication', 'Units': 's^2', "ValueType": "Scalar", "Obj_ID": ''}
+            ],
+            'Models': [
+                {
+                    'Name': 'm1',
+                    'Jobmanager': 'CVUT.demo01'
+                }
+            ]
+        }
+        super().__init__(metadata=MD)
+        self.updateMetadata(metadata)
+
+    def initialize(self, workdir='', metadata=None, validateMetaData=True, **kwargs):
+        super().initialize(workdir=workdir, metadata=metadata, validateMetaData=validateMetaData, **kwargs)
+
+    def get(self, objectTypeID, time=None, objectID=""):
+        return self.getModel('m1').get(objectTypeID=objectTypeID, time=time, objectID=objectID)
+
+    def set(self, obj, objectID=""):
+        self.getModel('m1').set(obj=obj, objectID=objectID)
+
+    def solveStep(self, tstep, stageID=0, runInBackground=False):
+        self.getModel('m1').solveStep(tstep=tstep, stageID=stageID, runInBackground=runInBackground)
+
+
+if __name__ == '__main__':
+    # with no targetTime and dt specified
+    # it leads to one-step simulation with default targetTime 1. s
+
+    value_1 = 3.
+    value_2 = 2.
+    delay = 1.
+
+    # inputs
+    param_1 = mp.ConstantProperty(value=value_1, propID=mp.DataID.PID_Time, valueType=mp.ValueType.Scalar, unit=mp.U.s, time=None)
+    param_2 = mp.ConstantProperty(value=value_2, propID=mp.DataID.PID_Time, valueType=mp.ValueType.Scalar, unit=mp.U.s, time=None)
+    param_delay = mp.ConstantProperty(value=delay, propID=mp.DataID.PID_Time, valueType=mp.ValueType.Scalar, unit=mp.U.s, time=None)
+
+    workflow = Workflow13()
+
+    # these metadata are supposed to be filled before execution
+    md = {
+        'Execution': {
+            'ID': 'N/A',
+            'Use_case_ID': 'N/A',
+            'Task_ID': 'N/A'
+        }
+    }
+    workflow.initialize(metadata=md)
+
+    # set the input values to the workfow which passes it to the models
+    workflow.set(param_1, objectID='1')
+    workflow.set(param_2, objectID='2')
+    workflow.set(param_delay, objectID='delay')
+
+    workflow.solve()
+    res_property = workflow.get(mp.DataID.PID_Time, 1.*mp.U.s)
+    value_result = res_property.inUnitsOf(mp.U.s*mp.U.s).getValue()
+    workflow.terminate()
+
+    print('Simulation has finished.')
+    print('Calculated result of %f x %f = %f' % (value_1, value_2, value_result))
+
+    if abs(value_result - 6.) <= 1.e-8:
+        log.info("Test OK")
+    else:
+        log.error("Test FAILED")
+        sys.exit(1)
--- mupifDB/api/main.py
+++ mupifDB/api/main.py
@@ -915,7 +915,7 @@ def db_init():
     if 'Settings' in db.list_collection_names(): return False
     for coll,rec in [
         ('Settings',{'projectName':'TEST','projectLogoUrl':'https://raw.githubusercontent.com/mupif/mupifDB/bd297a4a719336cd9672cfe73f31f7cbe2b4e029/webapi/static/images/mupif-logo.png'}),
-        ('UseCases',models.UseCase_Model(ucid='1',Description='Test usecase').model_dump()),
+        ('UseCases',models.UseCase_Model(ucid='Demo',Description='Demo usecase').model_dump()),
         ('Stat',models.MupifDBStatus_Model.Stat_Model().model_dump(mode="json")),
         ('Workflows',None),
         ('WorkflowsHistory',None),
@@ -923,11 +923,21 @@ def db_init():
         ('IOData',None)
     ]:
         try:
-            c=db.create_collection(coll)
-            if rec is None: continue
-            try: c.insert_one(rec)
-            except Exception: log.exception(f'Error populating initial collection {coll} with {rec}.')
-        except Exception as e: log.exception(f'Error creating initial collection {coll}.')
+            c = db.create_collection(coll)
+            if rec is None:
+                continue
+            try:
+                c.insert_one(rec)
+            except Exception:
+                log.exception(f'Error populating initial collection {coll} with {rec}.')
+        except Exception as e:
+            log.exception(f'Error creating initial collection {coll}.')
+    try:
+        from mupifDB import restApiControl
+        restApiControl.postWorkflowFiles('Demo', os.path.dirname(os.path.abspath(__file__))+'/demo/w13.py', [])
+
+    except Exception as e:
+        log.exception(f'Error: {e}.')
     return True
 
 
