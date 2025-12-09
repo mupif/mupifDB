84e805708db431132b1b33360cf7bd349f6b0a07
Stanislav Šulc <sulcstanda@seznam.cz>
2025-12-09 02:28:31 +0100

    openapi.json location

--- mupifDB/api/main.py
+++ mupifDB/api/main.py
@@ -493,7 +493,7 @@ app = FastAPI(
     openapi_tags=tags_metadata,
     # root_path="/api",
     servers=servers_list,
-    openapi_url="/api/openapi.json"
+    # openapi_url="/api/openapi.json"
 )
 
 
