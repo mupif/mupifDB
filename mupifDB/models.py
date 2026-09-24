d3beff9c0588fe85bebe0c238a20934cdcb5c985
Stanislav Šulc <sulcstanda@seznam.cz>
2026-09-24 16:03:47 +0200

    edmEntityId in IODataRecordItem_Model

--- mupifDB/models.py
+++ mupifDB/models.py
@@ -148,6 +148,7 @@ class IODataRecordItem_Model(InputOutputBase_Model):
     FileID: Optional[str]=None
     Compulsory: Optional[bool]=False # deema: allow None
     Object: dict[str,Any]={}
+    edmEntityId: Optional[str] = None
     def TEMP_getChildren(self) -> List[Tuple[str,DbRef_Model]]:
         return [('FileID',DbRef_Model(where=where,id=id)) for where,id in [('fs.files',self.FileID)] if id is not None and id!='']
 
